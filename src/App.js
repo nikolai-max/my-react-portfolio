cb1f8650ba147bdf3fc17717ec4d965e31d4ee44
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-03-12 17:16:11 +0100

    Added a layout from antd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Layout } from 'antd';
+import { Typography } from 'antd';
+import SiderDemo from './Component/Layout'
 
 const { Header, Footer, Sider, Content } = Layout;
+const { Title } = Typography;
 
 const App = () => (
   <div className="App">
-    <Layout>
-    <Header>Header</Header>
-      <Layout>
-        <Sider>Sider</Sider>
-        <Layout>
-          <Content>Content</Content>
-          <Footer>Footer</Footer>
-        </Layout>
-      </Layout>
-    </Layout>
+    <SiderDemo></SiderDemo>
   </div>
 );
 
