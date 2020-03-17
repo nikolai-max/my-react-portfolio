1de0cffda723b8e4a385df20668ab76d5682a7f8
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-03-17 15:37:02 +0100

    Changed the size of the tractor logo depended on screen width

--- src/Component/projectfolder/Getatractor.js
+++ src/Component/projectfolder/Getatractor.js
@@ -60,10 +60,13 @@ class Getatractor extends Component {
           <Tablet><img src={getatractor} className="container-image-tablet" alt="A mobile with the 'Get a tractor' application interface" /></Tablet>
           <Desktop><img src={getatractor} className="container-image-desktop" alt="A mobile with the 'Get a tractor' application interface" /></Desktop>
 
-          <p>Get a tractor was part of my 5 days 'markedplace' week in the Le Wagon code bootcamp. The ass is made with love together with my partner, Dennis Neely.</p>
           <p>Norway is full of unused tractors, and Get a tractor get them used by being the worlds first markedplace for swapping tractors.
           Through the app, you can hire out our tractor, or you can instantly hire a tractor.</p>
-          <img src={tractorlogo} className="logo-adjustments" alt="Get a tractor logo" />
+          <p>Get a tractor was part of my 5 days 'markedplace' week in the Le Wagon code bootcamp. The ass is made with love together with my partner, Dennis Neely.</p>
+
+          <Mobile><img src={tractorlogo} className="logo-adjustments-mobile" alt="A mobile with the 'Get a tractor' application interface" /></Mobile>
+          <Tablet><img src={tractorlogo} className="logo-adjustments-tablet" alt="A mobile with the 'Get a tractor' application interface" /></Tablet>
+          <Desktop><img src={tractorlogo} className="logo-adjustments-desktop" alt="A mobile with the 'Get a tractor' application interface" /></Desktop>
           <Button type="link" block href="https://github.com/portlandnorway/airtractor" target="_blank" >
             <FontAwesomeIcon icon={faGithub} />&nbsp;GitHub
           </Button>
